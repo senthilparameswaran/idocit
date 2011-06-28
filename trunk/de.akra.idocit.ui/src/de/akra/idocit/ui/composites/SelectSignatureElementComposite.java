13bd63e2363578d578ecd3522b3378dc17ab7cf9
dirk.meier.eickhoff <dirk.meier.eickhoff@165fa983-f9fe-27a1-bfdc-37165582f702>
2011-06-28 11:03:44 +0000

    Issue #6: Replaced Tree with TreeViewer and added context menu to expand and collapse the tree.

--- trunk/de.akra.idocit.ui/src/de/akra/idocit/ui/composites/SelectSignatureElementComposite.java
+++ trunk/de.akra.idocit.ui/src/de/akra/idocit/ui/composites/SelectSignatureElementComposite.java
@@ -15,20 +15,29 @@
  *******************************************************************************/
 package de.akra.idocit.ui.composites;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Tree;
-import org.eclipse.swt.widgets.TreeItem;
-import org.eclipse.swt.widgets.Widget;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.pocui.core.actions.EmptyActionConfiguration;
 import org.pocui.core.composites.CompositeInitializationException;
 import org.pocui.swt.composites.AbsComposite;
@@ -41,7 +50,7 @@ import de.akra.idocit.core.structure.Parameters;
 import de.akra.idocit.core.structure.SignatureElement;
 
 /**
- * The composite with a {@link Tree} in which the {@link SignatureElement} can be
+ * The composite with a {@link TreeViewer} in which the {@link SignatureElement} can be
  * selected, which the user want to document.
  * 
  * @author Dirk Meier-Eickhoff
@@ -56,11 +65,18 @@ public class SelectSignatureElementComposite
 	/**
 	 * Logger.
 	 */
-	private static Logger logger = Logger.getLogger(SelectSignatureElementComposite.class
+	private static Logger log = Logger.getLogger(SelectSignatureElementComposite.class
 			.getName());
 
-	private Tree selectionTree;
-	private SelectionListener selectionListener;
+	private TreeViewer selectionTreeViewer;
+	private ISelectionChangedListener selectionChangedListener;
+
+	private Menu treePopUpMenu;
+	private MenuItem menuItemExpandTree;
+	private MenuItem menuItemCollapseTree;
+
+	private SelectionListener menuItemExpandTreeSelectionListener;
+	private SelectionListener menuItemCollapseTreeSelectionListener;
 
 	/**
 	 * @param pvParent
@@ -77,29 +93,75 @@ public class SelectSignatureElementComposite
 	@Override
 	protected void initGUI(Composite pvParent) throws CompositeInitializationException
 	{
+		// both layouts are needed to maximize the TreeViewer in the GUI;
+		// maybe check if it works in a better way.
 		GridLayoutFactory.fillDefaults().applyTo(this);
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(this);
-		selectionTree = new Tree(pvParent, SWT.VIRTUAL | SWT.HORIZONTAL | SWT.VERTICAL
-				| SWT.SINGLE);
-		GridDataFactory.fillDefaults().grab(true, true).applyTo(selectionTree);
+		this.setLayout(new FillLayout(SWT.VERTICAL));
+
+		selectionTreeViewer = new TreeViewer(this, SWT.VIRTUAL | SWT.HORIZONTAL
+				| SWT.VERTICAL | SWT.SINGLE);
+		selectionTreeViewer.setContentProvider(new SignatureElementContentProvider());
+		selectionTreeViewer.setLabelProvider(new SignatureElementLabelProvider());
+		// expand up to Operations as default
+		selectionTreeViewer.setAutoExpandLevel(2);
+
+		// add context menu to TreeViewer
+		treePopUpMenu = new Menu(this.getShell(), SWT.POP_UP);
+		menuItemExpandTree = new MenuItem(treePopUpMenu, SWT.PUSH);
+		menuItemExpandTree.setText("Expand subitems");
+		menuItemCollapseTree = new MenuItem(treePopUpMenu, SWT.CASCADE);
+		menuItemCollapseTree.setText("Collapse subitems");
+
+		selectionTreeViewer.getTree().setMenu(treePopUpMenu);
 	}
 
 	@Override
 	protected void initListener() throws CompositeInitializationException
 	{
-		selectionListener = new SelectionListener() {
+		selectionChangedListener = new ISelectionChangedListener() {
+
+			@Override
+			public void selectionChanged(SelectionChangedEvent e)
+			{
+				treeSelectionChanged(e);
+			}
+		};
+
+		menuItemExpandTreeSelectionListener = new SelectionListener() {
 
 			@Override
 			public void widgetSelected(SelectionEvent e)
 			{
-				treeSelectionChanged(e);
+				Object selObj = ((TreeSelection) selectionTreeViewer.getSelection())
+						.getFirstElement();
+				if (selObj != null)
+				{
+					selectionTreeViewer.expandToLevel(selObj, TreeViewer.ALL_LEVELS);
+				}
 			}
 
 			@Override
 			public void widgetDefaultSelected(SelectionEvent e)
+			{}
+		};
+
+		menuItemCollapseTreeSelectionListener = new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e)
 			{
-				treeSelectionChanged(e);
+				Object selObj = ((TreeSelection) selectionTreeViewer.getSelection())
+						.getFirstElement();
+				if (selObj != null)
+				{
+					selectionTreeViewer.collapseToLevel(selObj, TreeViewer.ALL_LEVELS);
+				}
 			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e)
+			{}
 		};
 	}
 
@@ -108,15 +170,14 @@ public class SelectSignatureElementComposite
 	 * {@link SelectSignatureElementComposite#fireChangeEvent()}.
 	 * 
 	 * @param evt
-	 *            The {@link SelectionEvent}.
+	 *            The {@link SelectionChangedEvent}.
 	 */
-	private void treeSelectionChanged(SelectionEvent evt)
+	private void treeSelectionChanged(SelectionChangedEvent evt)
 	{
 		SelectSignatureElementCompositeSelection selection = getSelection();
 
-		TreeItem item = (TreeItem) evt.item;
-		SignatureElement attachedSigElem = (SignatureElement) item
-				.getData(item.getText());
+		TreeSelection sel = (TreeSelection) evt.getSelection();
+		SignatureElement attachedSigElem = (SignatureElement) sel.getFirstElement();
 
 		// fire only if another item is selected
 		if (selection.getSelectedSignatureElement() != attachedSigElem)
@@ -139,167 +200,223 @@ public class SelectSignatureElementComposite
 					|| (oldInSelection != null && newInSelection.getInterfaceArtifact() != oldInSelection
 							.getInterfaceArtifact()))
 			{
-				TreeItem[] items = selectionTree.getItems();
-				for (int i = 0; i < items.length; i++)
-				{
-					items[i].dispose();
-				}
-
-				// init tree by first use
-				buildTree(selectionTree, newInSelection.getInterfaceArtifact());
+				log.log(Level.FINE, "Build tree of SignatureElements.");
+				selectionTreeViewer.setInput(newInSelection.getInterfaceArtifact());
 			}
 		}
-	}
-
-	/**
-	 * Builds a {@link Tree} from the structure of {@link InterfaceArtifact}.
-	 * 
-	 * @param parent
-	 *            The parent {@link Tree} or {@link TreeItem} for new {@link TreeItem}s.
-	 * @param sigElem
-	 *            The {@link SignatureElement} that should be added to the {@link Tree}.
-	 * @throws IllegalArgumentException
-	 *             if the <code>parent</code> is not a {@link Tree} or {@link TreeItem}.
-	 */
-	private void buildTree(Widget parent, SignatureElement sigElem)
-			throws IllegalArgumentException
-	{
-		if (sigElem != null)
-		{
-			TreeItem item = addItemToTree(parent, sigElem);
-
-			// build further tree structure
-			if (sigElem instanceof Parameter)
-			{
-				Parameter param = (Parameter) sigElem;
-				if (param.getComplexType() != null)
-				{
-					for (Parameter p : param.getComplexType())
-					{
-						buildTree(item, p);
-					}
-				}
-			}
-			else if (sigElem instanceof Operation)
-			{
-				Operation op = (Operation) sigElem;
-				buildTree(item, op.getInputParameters());
-				buildTree(item, op.getOutputParameters());
-
-				List<? extends Parameters> exceptions = op.getExceptions();
-				for (Parameters exception : exceptions)
-				{
-					buildTree(item, exception);
-				}
-			}
-			else if (sigElem instanceof Parameters)
-			{
-				Parameters paramList = (Parameters) sigElem;
-				if (paramList.getParameters() != null)
-				{
-					for (Parameter p : paramList.getParameters())
-					{
-						buildTree(item, p);
-					}
-				}
-			}
-			else if (sigElem instanceof Interface)
-			{
-				Interface interf = (Interface) sigElem;
-
-				if (interf.getOperations() != null)
-				{
-					for (Operation o : interf.getOperations())
-					{
-						buildTree(item, o);
-					}
-				}
-				if (interf.getInnerInterfaces() != null)
-				{
-					for (Interface i : interf.getInnerInterfaces())
-					{
-						buildTree(item, i);
-					}
-				}
-			}
-			else if (sigElem instanceof InterfaceArtifact)
-			{
-				InterfaceArtifact iArtifact = (InterfaceArtifact) sigElem;
-				if (iArtifact.getInterfaces() != null)
-				{
-					for (Interface i : iArtifact.getInterfaces())
-					{
-						buildTree(item, i);
-					}
-				}
-			}
-		}
-	}
-
-	/**
-	 * Make a {@link TreeItem} from <code>sigElem</code>, add it to the
-	 * <code>parent</code> and return the new TreeItem.
-	 * 
-	 * @param parent
-	 *            The parent {@link Tree} or {@link TreeItem}.
-	 * @param sigElem
-	 *            The {@link SignatureElement} to add to the tree.
-	 * @return the created TreeItem.
-	 * @throws IllegalArgumentException
-	 *             if <code>parent</code> is no {@link Tree} or {@link TreeItem}.
-	 */
-	private TreeItem addItemToTree(Widget parent, SignatureElement sigElem)
-			throws IllegalArgumentException
-	{
-		// cast parent to create new TreeItem
-		TreeItem item;
-		if (parent instanceof TreeItem)
-		{
-			item = new TreeItem((TreeItem) parent, SWT.NONE);
-		}
-		else if (parent instanceof Tree)
-		{
-			item = new TreeItem((Tree) parent, SWT.NONE);
-		}
-		else
-		{
-			String errMsg = "Not supported Widget for Tree: " + parent;
-			logger.log(Level.SEVERE, errMsg);
-			throw new IllegalArgumentException(errMsg);
-		}
-
-		item.setText(sigElem.getDisplayName());
-		item.setData(sigElem.getDisplayName(), sigElem);
-
-		Image icon = getResourceConfiguration().getImageForSignatureElement(sigElem);
-		
-		if(icon != null){
-			item.setImage(icon);
-		}
-		
-		return item;
 	}
 
 	@Override
 	protected void addAllListener()
 	{
-		selectionTree.addSelectionListener(selectionListener);
+		selectionTreeViewer.addSelectionChangedListener(selectionChangedListener);
+		menuItemCollapseTree.addSelectionListener(menuItemCollapseTreeSelectionListener);
+		menuItemExpandTree.addSelectionListener(menuItemExpandTreeSelectionListener);
 	}
 
 	@Override
 	protected void removeAllListener()
 	{
-		if (selectionTree.getListeners(SWT.Selection).length > 0)
-		{
-			selectionTree.removeSelectionListener(selectionListener);
-		}
+		selectionTreeViewer.removeSelectionChangedListener(selectionChangedListener);
+		menuItemCollapseTree.removeSelectionListener(menuItemCollapseTreeSelectionListener);
+		menuItemExpandTree.removeSelectionListener(menuItemExpandTreeSelectionListener);
 	}
 
 	@Override
 	public void doCleanUp()
 	{
-		selectionTree.removeAll();
 		SelectSignatureElementCompositeRC rc = getResourceConfiguration();
 		rc.disposeImageCache();
+	}
+
+	/**
+	 * The {@link ITreeContentProvider} for the
+	 * {@link SelectSignatureElementComposite#selectionTreeViewer}.
+	 * 
+	 * @author Dirk Meier-Eickhoff * @since 0.0.1
+	 * @version 0.0.1
+	 */
+	private static class SignatureElementContentProvider implements ITreeContentProvider
+	{
+		private static final Object[] EMPTY_ARRAY = new Object[0];
+
+		@Override
+		public void dispose()
+		{}
+
+		@Override
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput)
+		{}
+
+		@Override
+		public Object[] getElements(Object inputElement)
+		{
+			return getChildren(inputElement);
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public Object[] getChildren(Object parentElement)
+		{
+			Object[] children = EMPTY_ARRAY;
+			if (parentElement instanceof Parameter)
+			{
+				Parameter param = (Parameter) parentElement;
+				if (param.getComplexType() != null)
+				{
+					children = param.getComplexType().toArray();
+				}
+			}
+			else if (parentElement instanceof Operation)
+			{
+				Operation op = (Operation) parentElement;
+				List<SignatureElement> list = new ArrayList<SignatureElement>(2);
+				if (op.getInputParameters() != null)
+				{
+					list.add(op.getInputParameters());
+				}
+				if (op.getOutputParameters() != null)
+				{
+					list.add(op.getOutputParameters());
+				}
+				children = concat(list, op.getExceptions());
+			}
+			else if (parentElement instanceof Parameters)
+			{
+				Parameters paramList = (Parameters) parentElement;
+				children = paramList.getParameters().toArray();
+			}
+			else if (parentElement instanceof Interface)
+			{
+				Interface interf = (Interface) parentElement;
+				children = concat(interf.getOperations(), interf.getInnerInterfaces());
+			}
+			else if (parentElement instanceof InterfaceArtifact)
+			{
+				InterfaceArtifact iArtifact = (InterfaceArtifact) parentElement;
+				children = iArtifact.getInterfaces().toArray();
+			}
+			return children;
+		}
+
+		/**
+		 * Concatenates the <code>lists</code> of {@link SignatureElement}s in an array of
+		 * Objects.
+		 * 
+		 * @param lists
+		 *            The lists to concatenate. If a <code>lists</code>' item is
+		 *            <code>null</code> it is ignored.
+		 * @return the concatenated lists as new Object[].
+		 */
+		private Object[] concat(List<? extends SignatureElement>... lists)
+		{
+			Object[] result = EMPTY_ARRAY;
+			if (lists.length > 0)
+			{
+				// calculate total array size
+				int totalLength = 0;
+				for (int i = 0; i < lists.length; ++i)
+				{
+					List<? extends SignatureElement> item = lists[i];
+					if (item != null)
+					{
+						totalLength += item.size();
+					}
+				}
+
+				// copy elements to new list
+				ArrayList<SignatureElement> concatList = new ArrayList<SignatureElement>(
+						totalLength);
+				for (int i = 0; i < lists.length; ++i)
+				{
+					List<? extends SignatureElement> item = lists[i];
+					if (item != null)
+					{
+						concatList.addAll(item);
+					}
+				}
+
+				// convert list to array
+				if (!concatList.isEmpty())
+				{
+					result = concatList.toArray();
+				}
+			}
+			return result;
+		}
+
+		@Override
+		public Object getParent(Object element)
+		{
+			if (element instanceof SignatureElement
+					&& element != SignatureElement.EMPTY_SIGNATURE_ELEMENT)
+			{
+				return ((SignatureElement) element).getParent();
+			}
+			return null;
+		}
+
+		@Override
+		public boolean hasChildren(Object element)
+		{
+			return getChildren(element).length > 0;
+		}
+
+	}
+
+	/**
+	 * The {@link ILabelProvider} for the
+	 * {@link SelectSignatureElementComposite#selectionTreeViewer}.
+	 * 
+	 * @author Dirk Meier-Eickhoff * @since 0.0.1
+	 * @version 0.0.1
+	 */
+	private class SignatureElementLabelProvider implements ILabelProvider
+	{
+
+		@Override
+		public void addListener(ILabelProviderListener listener)
+		{}
+
+		@Override
+		public void dispose()
+		{
+			// do nothing. The images are managed in getResourceConfiguration()
+		}
+
+		@Override
+		public boolean isLabelProperty(Object element, String property)
+		{
+			// changes in the SignatureElements are never changes in their display names
+			// (only in their documentations)
+			return false;
+		}
+
+		@Override
+		public void removeListener(ILabelProviderListener listener)
+		{}
+
+		@Override
+		public Image getImage(Object element)
+		{
+			Image img = null;
+			if (element instanceof SignatureElement)
+			{
+				img = getResourceConfiguration().getImageForSignatureElement(
+						(SignatureElement) element);
+			}
+			return img;
+		}
+
+		@Override
+		public String getText(Object element)
+		{
+			String name = null;
+			if (element instanceof SignatureElement)
+			{
+				name = ((SignatureElement) element).getDisplayName();
+			}
+			return name;
+		}
 	}
 }
