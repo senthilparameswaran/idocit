91742967757ab531ede06b724cd526958f6cc8dc
krausedammann@web.de <krausedammann@web.de@165fa983-f9fe-27a1-bfdc-37165582f702>
2011-07-21 19:26:30 +0000

    Removed all WordNet and StanfordTagger-aspects from the Preference Page

--- trunk/de.akra.idocit.ui/src/de/akra/idocit/ui/components/IDocItPreferencePage.java
+++ trunk/de.akra.idocit.ui/src/de/akra/idocit/ui/components/IDocItPreferencePage.java
@@ -15,27 +15,17 @@
  *******************************************************************************/
 package de.akra.idocit.ui.components;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.MouseEvent;
-import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.browser.IWebBrowser;
-import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
-import org.eclipse.ui.internal.browser.WorkbenchBrowserSupport;
 
 import de.akra.idocit.core.IDocItActivator;
 
@@ -50,15 +40,6 @@ public class IDocItPreferencePage extends FieldEditorPreferencePage implements
 		IWorkbenchPreferencePage
 {
 
-	// Constants
-	private static final String WORDNET_REFERENCE_FILE_NAME = "index.sense";
-
-	// Widgets and field editors
-	private Label lblWordNetLink;
-
-	// Listeners
-	private MouseListener wordnetLinkMouseListener;
-
 	/**
 	 * Constructor.
 	 */
@@ -71,64 +52,13 @@ public class IDocItPreferencePage extends FieldEditorPreferencePage implements
 	public void dispose()
 	{
 		super.dispose();
-		lblWordNetLink.removeMouseListener(wordnetLinkMouseListener);
 	}
 
 	@Override
 	protected Control createContents(Composite parent)
 	{
 		Label lblWordNet = new Label(parent, SWT.NONE);
-		lblWordNet.setText("Please specify where WordNet is installed and the PoS-Tagger Model is stored on your computer.");
-
-		lblWordNetLink = new Label(parent, SWT.NONE);
-		lblWordNetLink.setText("If you do not have Wordnet, download it here!");
-		lblWordNetLink.setForeground(getShell().getDisplay().getSystemColor(
-				SWT.COLOR_BLUE));
-		lblWordNetLink.setCursor(Display.getCurrent().getSystemCursor(SWT.CURSOR_HAND));
-
-		wordnetLinkMouseListener = new MouseListener() {
-			@Override
-			public void mouseUp(MouseEvent e)
-			{
-				int style = IWorkbenchBrowserSupport.AS_EDITOR
-						| IWorkbenchBrowserSupport.LOCATION_BAR
-						| IWorkbenchBrowserSupport.STATUS;
-				try
-				{
-					IWebBrowser browser = WorkbenchBrowserSupport.getInstance()
-							.createBrowser(style, "WordNet-Download", "WordNet-Download",
-									"Download WordNet");
-					browser.openURL(new URL(
-							"http://wordnet.princeton.edu/wordnet/download/"));
-				}
-				catch (PartInitException e1)
-				{
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				catch (MalformedURLException e2)
-				{
-					// TODO Auto-generated catch block
-					e2.printStackTrace();
-				}
-
-			}
-
-			@Override
-			public void mouseDown(MouseEvent e)
-			{
-				// Nothing to do!
-			}
-
-			@Override
-			public void mouseDoubleClick(MouseEvent e)
-			{
-				// Nothing to do!
-			}
-		};
-
-		lblWordNetLink.addMouseListener(wordnetLinkMouseListener);
-		new Label(parent, SWT.NONE);
+		lblWordNet.setText("Welcome to the customization area of iDocIt!");
 
 		return super.createContents(parent);
 	}
