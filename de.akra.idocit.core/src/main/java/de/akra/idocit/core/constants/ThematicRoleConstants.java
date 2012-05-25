6def3b6e8217510121e724fccfcd9f14a937d8d1
krausedammann <krausedammann@web.de>
2012-05-25 11:16:10 +0000

    Fix issue #96: Class descriptions are now visualized with thematic role "NONE" in the iDocIt! GUI. When the artifact is saved, the class description will appear without a thematic role.

---
+++ de.akra.idocit.core/src/main/java/de/akra/idocit/core/constants/ThematicRoleConstants.java
@@ -0,0 +1,11 @@
+package de.akra.idocit.core.constants;
+
+public final class ThematicRoleConstants
+{
+	public static String MANDATORY_ROLE_ACTION = "ACTION";
+	public static String MANDATORY_ROLE_RULE = "RULE";
+	public static String MANDATORY_ROLE_NONE = "NONE";
+
+	public static String[] MANDARTORY_ROLES = new String[] { MANDATORY_ROLE_ACTION,
+			MANDATORY_ROLE_NONE, MANDATORY_ROLE_RULE };
+}
