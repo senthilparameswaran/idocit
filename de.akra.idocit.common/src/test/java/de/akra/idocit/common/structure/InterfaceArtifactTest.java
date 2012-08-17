8d733a3fbeed1aa56f005401bebb87ec7048abb2
krausedammann <krausedammann@web.de>
2012-08-17 10:02:34 +0000

    Extend method for thematic role collection with special care about error cases (incl. tests) for issue #117

--- de.akra.idocit.common/src/test/java/de/akra/idocit/common/structure/InterfaceArtifactTest.java
+++ de.akra.idocit.common/src/test/java/de/akra/idocit/common/structure/InterfaceArtifactTest.java
@@ -16,18 +16,13 @@
 package de.akra.idocit.common.structure;
 
 import static org.junit.Assert.assertEquals;
-import java.util.List;
-import java.util.Vector;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.junit.Test;
 
-import de.akra.idocit.common.structure.impl.TestInterface;
-import de.akra.idocit.common.structure.impl.TestInterfaceArtifact;
-import de.akra.idocit.common.structure.impl.TestOperation;
-import de.akra.idocit.common.structure.impl.TestParameter;
-import de.akra.idocit.common.structure.impl.TestParameters;
+import de.akra.idocit.common.utils.TestUtils;
 
 /**
  * Tests for {@link InterfaceArtifact}.
@@ -52,7 +47,7 @@ public class InterfaceArtifactTest
 		 * ******************************************************************************
 		 */
 		{
-			InterfaceArtifact sourceArtifact = createInterfaceArtifact();
+			InterfaceArtifact sourceArtifact = TestUtils.createInterfaceArtifact();
 
 			InterfaceArtifact copiedArtifact = (InterfaceArtifact) sourceArtifact
 					.copy(SignatureElement.EMPTY_SIGNATURE_ELEMENT);
@@ -72,7 +67,7 @@ public class InterfaceArtifactTest
 		 * ****************************************************************************
 		 */
 		{
-			InterfaceArtifact sourceArtifact = createInterfaceArtifact();
+			InterfaceArtifact sourceArtifact = TestUtils.createInterfaceArtifact();
 
 			InterfaceArtifact copiedArtifact = (InterfaceArtifact) sourceArtifact
 					.copy(SignatureElement.EMPTY_SIGNATURE_ELEMENT);
@@ -86,78 +81,5 @@ public class InterfaceArtifactTest
 			boolean res = sourceArtifact.equals(copiedArtifact);
 			assertEquals(false, res);
 		}
-	}
-
-	/**
-	 * Create a test InterfaceArtifact.
-	 * 
-	 * @return a new InterfaceArtifact with some constant values.
-	 */
-	private InterfaceArtifact createInterfaceArtifact()
-	{
-		InterfaceArtifact artifact = new TestInterfaceArtifact(
-				SignatureElement.EMPTY_SIGNATURE_ELEMENT, "Artifact", Numerus.SINGULAR);
-		artifact.setIdentifier("test.wsdl");
-
-		List<Interface> interfaceList = new Vector<Interface>();
-		Interface interf = new TestInterface(artifact, "PortType", Numerus.SINGULAR);
-		interf.setIdentifier("CustomerService");
-		interfaceList.add(interf);
-
-		List<Operation> operations = new Vector<Operation>();
-		Operation op = new TestOperation(interf, "Operation", "Searching Operations", Numerus.SINGULAR);
-		op.setIdentifier("find");
-		op.setDocumentationChanged(true);
-		operations.add(op);
-		interf.setOperations(operations);
-
-		/*
-		 * Input message
-		 */
-		Parameters inputParameters = new TestParameters(op, "InputMessage", Numerus.SINGULAR);
-		inputParameters.setIdentifier("findIn");
-		op.setInputParameters(inputParameters);
-
-		Parameter paramCust = new TestParameter(inputParameters, "Part", Numerus.SINGULAR, true);
-		paramCust.setIdentifier("Cust");
-		paramCust.setDataTypeName("Customer");
-		inputParameters.addParameter(paramCust);
-
-		Parameter paramId = new TestParameter(paramCust, "", Numerus.SINGULAR, false);
-		paramId.setIdentifier("id");
-		paramId.setDataTypeName("int");
-		paramCust.addParameter(paramId);
-
-		Parameter paramNameIn = new TestParameter(paramCust, "", Numerus.SINGULAR, false);
-		paramNameIn.setIdentifier("name");
-		paramNameIn.setDataTypeName("String");
-		paramCust.addParameter(paramNameIn);
-
-		/*
-		 * Output message
-		 */
-		Parameters outputParameters = new TestParameters(op, "OutputMessage", Numerus.SINGULAR);
-		outputParameters.setIdentifier("findOut");
-		op.setOutputParameters(outputParameters);
-
-		Parameter paramCustOut = new TestParameter(outputParameters, "Part", Numerus.SINGULAR, true);
-		paramCustOut.setIdentifier("Cust");
-		paramCustOut.setDataTypeName("Customer");
-		outputParameters.addParameter(paramCust);
-
-		Parameter paramIdOut = new TestParameter(paramCustOut, "", Numerus.SINGULAR, false);
-		paramIdOut.setIdentifier("id");
-		paramIdOut.setDataTypeName("int");
-		paramIdOut.setDocumentationChanged(true);
-		paramCustOut.addParameter(paramIdOut);
-
-		Parameter paramNameOut = new TestParameter(paramCustOut, "", Numerus.SINGULAR, false);
-		paramNameOut.setIdentifier("name");
-		paramNameOut.setDataTypeName("String");
-		paramCustOut.addParameter(paramNameOut);
-
-		artifact.setInterfaces(interfaceList);
-
-		return artifact;
 	}
 }
