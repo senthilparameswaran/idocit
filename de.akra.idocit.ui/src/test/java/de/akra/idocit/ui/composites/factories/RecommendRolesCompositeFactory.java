033272c3201cdcda23be92a0c5ca8660888919ac
krausedammann <krausedammann@web.de>
2012-08-17 11:08:12 +0000

    Extended handling of new error case checkbox in UIs (due to issue #117)

--- de.akra.idocit.ui/src/test/java/de/akra/idocit/ui/composites/factories/RecommendRolesCompositeFactory.java
+++ de.akra.idocit.ui/src/test/java/de/akra/idocit/ui/composites/factories/RecommendRolesCompositeFactory.java
@@ -15,38 +15,70 @@
  *******************************************************************************/
 package de.akra.idocit.ui.composites.factories;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.pocui.core.actions.EmptyActionConfiguration;
-import org.pocui.core.resources.EmptyResourceConfiguration;
 import org.pocui.swt.composites.AbsComposite;
 import org.pocui.swt.composites.ICompositeFactory;
 
+import de.akra.idocit.common.structure.ThematicRole;
 import de.akra.idocit.core.services.impl.ServiceManager;
 import de.akra.idocit.ui.composites.RecommendRolesComposite;
+import de.akra.idocit.ui.composites.RecommendRolesCompositeRC;
 import de.akra.idocit.ui.composites.RecommendRolesCompositeSelection;
 import de.akra.idocit.ui.services.CompositeTestPersistenceService;
 
 public class RecommendRolesCompositeFactory
 		implements
-		ICompositeFactory<EmptyActionConfiguration, EmptyResourceConfiguration, RecommendRolesCompositeSelection>
+		ICompositeFactory<EmptyActionConfiguration, RecommendRolesCompositeRC, RecommendRolesCompositeSelection>
 {
 
 	@Override
-	public AbsComposite<EmptyActionConfiguration, EmptyResourceConfiguration, RecommendRolesCompositeSelection> createComposite(
+	public AbsComposite<EmptyActionConfiguration, RecommendRolesCompositeRC, RecommendRolesCompositeSelection> createComposite(
 			Composite parent, int style)
 	{
 		ServiceManager.getInstance().setPersistenceService(
 				new CompositeTestPersistenceService());
 
 		final RecommendRolesComposite composite = new RecommendRolesComposite(parent,
-				style);
-		composite.setSelection(new RecommendRolesCompositeSelection());
+				style, getResourceConfiguration());
+		final RecommendRolesCompositeSelection selection = new RecommendRolesCompositeSelection();
+
+		List<ThematicRole> thematicRoles = new ArrayList<ThematicRole>();
+		ThematicRole roleAgent = new ThematicRole("AGENT");
+		ThematicRole roleAction = new ThematicRole("ACTION");
+		thematicRoles.add(roleAgent);
+		thematicRoles.add(roleAction);
+
+		Set<ThematicRole> assignedThematicRoles = new TreeSet<ThematicRole>();
+		assignedThematicRoles.add(thematicRoles.get(0));
+
+		final Set<ThematicRole> assignedThematicRolesDocumentingErrors = new TreeSet<ThematicRole>();
+		assignedThematicRolesDocumentingErrors.add(roleAction);
+
+		selection.setOperationIdentifier("findCustomerById");
+		selection.setAssignedThematicRoles(assignedThematicRoles);
+		selection
+				.setAssignedThematicRolesWithErrorDocs(assignedThematicRolesDocumentingErrors);
+		selection.setReferenceThematicGridName("Searching Operations");
+		
+		composite.setSelection(selection);
 		return composite;
 	}
 
 	@Override
-	public EmptyResourceConfiguration getResourceConfiguration()
+	public RecommendRolesCompositeRC getResourceConfiguration()
 	{
-		return EmptyResourceConfiguration.getInstance();
+		RecommendRolesCompositeRC resConf = new RecommendRolesCompositeRC();
+		resConf.setRoleWithoutErrorDocsWarningIcon(new Image(Display.getDefault(),
+				"src/test/resources/de/akra/idocit/ui/composites/factories/int_obj.gif"));
+
+		return resConf;
 	}
 }
