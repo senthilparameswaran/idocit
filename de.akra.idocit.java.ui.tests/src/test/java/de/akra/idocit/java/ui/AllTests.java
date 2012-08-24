8bc44beab3a898936f9e4e2752dbc924b01490fb
krausedammann <krausedammann@web.de>
2012-08-24 09:15:02 +0000

    Initial commit

---
+++ de.akra.idocit.java.ui.tests/src/test/java/de/akra/idocit/java/ui/AllTests.java
@@ -0,0 +1,12 @@
+package de.akra.idocit.java.ui;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ JavaEditorSelectionListenerTest.class })
+public class AllTests
+{
+
+}
