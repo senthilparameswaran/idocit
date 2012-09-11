3e105698a01192fbeee21961ea8876a93a0fc641
dirk.meier.eickhoff <dirk.meier.eickhoff@gmail.com>
2012-09-11 08:28:59 +0000

    Added license information and reverted the extraction of the core tests because of dependency problems

---
+++ de.akra.idocit.core/src/test/java/de/akra/idocit/core/services/HTMLDocGeneratorTest.java
@@ -0,0 +1,183 @@
+/*******************************************************************************
+ * Copyright 2012 AKRA GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package de.akra.idocit.core.services;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import org.junit.Test;
+
+import de.akra.idocit.common.structure.Addressee;
+import de.akra.idocit.common.structure.Delimiters;
+import de.akra.idocit.common.structure.Documentation;
+import de.akra.idocit.common.structure.Interface;
+import de.akra.idocit.common.structure.InterfaceArtifact;
+import de.akra.idocit.common.structure.Numerus;
+import de.akra.idocit.common.structure.Operation;
+import de.akra.idocit.common.structure.Parameter;
+import de.akra.idocit.common.structure.Parameters;
+import de.akra.idocit.common.structure.SignatureElement;
+import de.akra.idocit.common.structure.ThematicRole;
+import de.akra.idocit.common.structure.impl.TestInterface;
+import de.akra.idocit.common.structure.impl.TestInterfaceArtifact;
+import de.akra.idocit.common.structure.impl.TestOperation;
+import de.akra.idocit.common.structure.impl.TestParameter;
+import de.akra.idocit.common.structure.impl.TestParameters;
+import de.akra.idocit.core.services.impl.HTMLDocGenerator;
+
+/**
+ * Tests for {@link HTMLDocGenerator}.
+ * 
+ * @author Dirk Meier-Eickhoff
+ * 
+ */
+public class HTMLDocGeneratorTest
+{
+	/**
+	 * The delimiters for WSDL.
+	 */
+	private static final Delimiters delimiters;
+
+	/**
+	 * Initialize <code>delimiters</code>.
+	 */
+	static
+	{
+		// delimiters for WSDL
+		delimiters = new Delimiters();
+		delimiters.pathDelimiter = ";";
+		// TODO maybe '}' as namespace delimiter?
+		delimiters.namespaceDelimiter = "#";
+		delimiters.typeDelimiter = "+";
+	}
+	
+	@Test
+	public void testGenerateHTML()
+	{
+		InterfaceArtifact artifact= createInterfaceArtifact();
+		HTMLDocGenerator docGen = new HTMLDocGenerator(artifact, delimiters);
+		System.out.println(docGen.generateHTML());
+	}
+
+	
+	/**
+	 * Create a test InterfaceArtifact.
+	 * 
+	 * @return a new InterfaceArtifact with some constant values.
+	 */
+	private InterfaceArtifact createInterfaceArtifact()
+	{
+		InterfaceArtifact artifact = new TestInterfaceArtifact(
+				SignatureElement.EMPTY_SIGNATURE_ELEMENT, "Artifact", Numerus.SINGULAR);
+		artifact.setIdentifier("test.wsdl");
+
+		List<Interface> interfaceList = new Vector<Interface>();
+		Interface interf = new TestInterface(artifact, "PortType", Numerus.SINGULAR) {
+		};
+		interf.setIdentifier("CustomerService");
+		interf.addDocpart(createDocumentation());
+		interfaceList.add(interf);
+
+		List<Operation> operations = new Vector<Operation>();
+		Operation op = new TestOperation(interf, "Operation", "Searching Operations", Numerus.SINGULAR);
+		op.setIdentifier("find");
+		op.addDocpart(createDocumentation());
+		operations.add(op);
+		interf.setOperations(operations);
+
+		/*
+		 * Input message
+		 */
+		Parameters inputParameters = new TestParameters(op, "InputMessage", Numerus.SINGULAR);
+		inputParameters.setIdentifier("findIn");
+		inputParameters.addDocpart(createDocumentation());
+		op.setInputParameters(inputParameters);
+
+		Parameter paramCust = new TestParameter(inputParameters, "Part", Numerus.SINGULAR, true);
+		paramCust.setIdentifier("Cust");
+		paramCust.setDataTypeName("Customer");
+		inputParameters.addParameter(paramCust);
+
+		Parameter paramId = new TestParameter(paramCust, "", Numerus.SINGULAR, false);
+		paramId.setIdentifier("id");
+		paramId.setDataTypeName("int");
+		paramCust.addParameter(paramId);
+
+		Parameter paramNameIn = new TestParameter(paramCust, "", Numerus.SINGULAR, false);
+		paramNameIn.setIdentifier("name");
+		paramNameIn.setDataTypeName("String");
+		paramCust.addParameter(paramNameIn);
+
+		/*
+		 * Output message
+		 */
+		Parameters outputParameters = new TestParameters(op, "OutputMessage", Numerus.SINGULAR);
+		outputParameters.setIdentifier("findOut");
+		op.setOutputParameters(outputParameters);
+
+		Parameter paramCustOut = new TestParameter(outputParameters, "Part", Numerus.SINGULAR, true);
+		paramCustOut.setIdentifier("Cust");
+		paramCustOut.setDataTypeName("Customer");
+		outputParameters.addParameter(paramCust);
+
+		Parameter paramIdOut = new TestParameter(paramCustOut, "", Numerus.SINGULAR, false);
+		paramIdOut.setIdentifier("id");
+		paramIdOut.setDataTypeName("int");
+		paramCustOut.addParameter(paramIdOut);
+
+		Parameter paramNameOut = new TestParameter(paramCustOut, "", Numerus.SINGULAR, false);
+		paramNameOut.setIdentifier("name");
+		paramNameOut.setDataTypeName("String");
+		paramCustOut.addParameter(paramNameOut);
+
+		artifact.setInterfaces(interfaceList);
+
+		return artifact;
+	}
+	
+	/**
+	 * Create a test Documentation.
+	 * 
+	 * @return a new Documentation with some constant values.
+	 */
+	public static Documentation createDocumentation()
+	{
+		Documentation newDoc = new Documentation();
+		newDoc.setThematicRole(new ThematicRole("OBJECT"));
+
+		newDoc.setSignatureElementIdentifier("test.wsdl.CustomerService.find.input(Customer).Customer");
+
+		Map<Addressee, String> docMap = new HashMap<Addressee, String>();
+		List<Addressee> addresseeSequence = new LinkedList<Addressee>();
+
+		Addressee developer = new Addressee("DEVELOPER");
+		Addressee manager = new Addressee("MANAGER");
+
+		docMap.put(developer, "Documenation for developers.");
+		addresseeSequence.add(developer);
+
+		docMap.put(manager, "Documenation for managers.");
+		addresseeSequence.add(manager);
+
+		newDoc.setDocumentation(docMap);
+		newDoc.setAddresseeSequence(addresseeSequence);
+
+		return newDoc;
+	}
+}
