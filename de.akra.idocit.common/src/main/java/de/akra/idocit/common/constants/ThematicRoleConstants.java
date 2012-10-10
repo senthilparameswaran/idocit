911a79f9a8d6853e65b3b521266f29cec43c1dd0
krausedammann <krausedammann@web.de>
2012-10-10 18:59:58 +0000

    Fix findbugs-warning: final static mutable array

--- de.akra.idocit.common/src/main/java/de/akra/idocit/common/constants/ThematicRoleConstants.java
+++ de.akra.idocit.common/src/main/java/de/akra/idocit/common/constants/ThematicRoleConstants.java
@@ -15,13 +15,18 @@
  *******************************************************************************/
 package de.akra.idocit.common.constants;
 
-public final class ThematicRoleConstants
-{
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public final class ThematicRoleConstants {
 	public static String MANDATORY_ROLE_ACTION = "ACTION";
 	public static String MANDATORY_ROLE_RULE = "RULE";
 	public static String MANDATORY_ROLE_NONE = "NONE";
 	public static String MANDATORY_ROLE_AGENT = "AGENT";
 
-	public static String[] MANDARTORY_ROLES = new String[] { MANDATORY_ROLE_ACTION,
-			MANDATORY_ROLE_NONE, MANDATORY_ROLE_RULE, MANDATORY_ROLE_AGENT };
+	public static List<String> MANDARTORY_ROLES = Collections
+			.unmodifiableList(Arrays.asList(new String[] {
+					MANDATORY_ROLE_ACTION, MANDATORY_ROLE_NONE,
+					MANDATORY_ROLE_RULE, MANDATORY_ROLE_AGENT }));
 }
