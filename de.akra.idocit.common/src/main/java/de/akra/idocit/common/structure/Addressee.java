ea3595c67a6dac45ab8656bdc6dce1c211608ce0
krausedammann <krausedammann@web.de>
2012-10-11 16:49:41 +0000

    Add super.clone() invocation to clone-method of Addressee

--- de.akra.idocit.common/src/main/java/de/akra/idocit/common/structure/Addressee.java
+++ de.akra.idocit.common/src/main/java/de/akra/idocit/common/structure/Addressee.java
@@ -22,8 +22,7 @@ package de.akra.idocit.common.structure;
  * @since 0.0.1
  * @version 0.0.1
  */
-public class Addressee implements DescribedItem, Cloneable
-{
+public class Addressee implements DescribedItem, Cloneable {
 	private String name = "";
 
 	private String description = "";
@@ -33,37 +32,32 @@ public class Addressee implements DescribedItem, Cloneable
 	/**
 	 * Default constructor. It is needed by XStream.
 	 */
-	public Addressee()
-	{}
-	
+	public Addressee() {
+	}
+
 	/**
 	 * Constructor.
 	 * 
 	 * @param name
 	 *            The name of the addressee group.
 	 */
-	public Addressee(String name)
-	{
+	public Addressee(String name) {
 		this.name = name;
 	}
 
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
 
-	public void setName(String name)
-	{
+	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getDescription()
-	{
+	public String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description)
-	{
+	public void setDescription(String description) {
 		this.description = description;
 	}
 
@@ -71,16 +65,14 @@ public class Addressee implements DescribedItem, Cloneable
 	 * @param isDefault
 	 *            the isDefault to set
 	 */
-	public void setDefault(boolean isDefault)
-	{
+	public void setDefault(boolean isDefault) {
 		this.isDefault = isDefault;
 	}
 
 	/**
 	 * @return the isDefault
 	 */
-	public boolean isDefault()
-	{
+	public boolean isDefault() {
 		return isDefault;
 	}
 
@@ -90,11 +82,11 @@ public class Addressee implements DescribedItem, Cloneable
 	 * @see java.lang.Object#hashCode()
 	 */
 	@Override
-	public int hashCode()
-	{
+	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
 		result = prime * result + (isDefault ? 1231 : 1237);
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
@@ -106,45 +98,32 @@ public class Addressee implements DescribedItem, Cloneable
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
 	@Override
-	public boolean equals(Object obj)
-	{
-		if (this == obj)
-		{
+	public boolean equals(Object obj) {
+		if (this == obj) {
 			return true;
 		}
-		if (obj == null)
-		{
+		if (obj == null) {
 			return false;
 		}
-		if (!(obj instanceof Addressee))
-		{
+		if (!(obj instanceof Addressee)) {
 			return false;
 		}
 		Addressee other = (Addressee) obj;
-		if (description == null)
-		{
-			if (other.description != null)
-			{
+		if (description == null) {
+			if (other.description != null) {
 				return false;
 			}
-		}
-		else if (!description.equals(other.description))
-		{
+		} else if (!description.equals(other.description)) {
 			return false;
 		}
-		if (isDefault != other.isDefault)
-		{
+		if (isDefault != other.isDefault) {
 			return false;
 		}
-		if (name == null)
-		{
-			if (other.name != null)
-			{
+		if (name == null) {
+			if (other.name != null) {
 				return false;
 			}
-		}
-		else if (!name.equals(other.name))
-		{
+		} else if (!name.equals(other.name)) {
 			return false;
 		}
 		return true;
@@ -156,24 +135,28 @@ public class Addressee implements DescribedItem, Cloneable
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("Addressee [name=");
 		builder.append(name);
-		//builder.append(", description=");
-		///builder.append(description);
-		//builder.append(", isDefault=");
-		//builder.append(isDefault);
+		// builder.append(", description=");
+		// /builder.append(description);
+		// builder.append(", isDefault=");
+		// builder.append(isDefault);
 		builder.append("]");
 		return builder.toString();
 	}
 
-	public Addressee clone()
-	{
-		Addressee clone = new Addressee(getName());
-		clone.setDescription(getDescription());
-		clone.setDefault(isDefault());
-		return clone;
+	@Override
+	public Addressee clone() {
+		try {
+			Addressee clone = (Addressee) super.clone();
+			clone.setName(getName());
+			clone.setDescription(getDescription());
+			clone.setDefault(isDefault());
+			return clone;
+		} catch (CloneNotSupportedException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
