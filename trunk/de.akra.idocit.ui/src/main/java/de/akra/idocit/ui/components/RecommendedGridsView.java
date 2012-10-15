643025fc1419011e45db7d9db570e5831079e4ef
krausedammann@web.de <krausedammann@web.de@165fa983-f9fe-27a1-bfdc-37165582f702>
2012-10-15 19:12:24 +0000

    Remove unused labelprovider

--- trunk/de.akra.idocit.ui/src/main/java/de/akra/idocit/ui/components/RecommendedGridsView.java
+++ trunk/de.akra.idocit.ui/src/main/java/de/akra/idocit/ui/components/RecommendedGridsView.java
@@ -17,13 +17,8 @@ package de.akra.idocit.ui.components;
 
 import java.util.HashSet;
 
-import org.eclipse.jface.viewers.ITableLabelProvider;
-import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.ISharedImages;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
 import de.akra.idocit.common.structure.ThematicGrid;
@@ -49,25 +44,6 @@ public class RecommendedGridsView extends ViewPart
 	public static final String ID = RecommendedGridsView.class.getName();
 
 	private RecommendRolesComposite recommendRolesComposite;
-
-	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider
-	{
-		public String getColumnText(Object obj, int index)
-		{
-			return getText(obj);
-		}
-
-		public Image getColumnImage(Object obj, int index)
-		{
-			return getImage(obj);
-		}
-
-		public Image getImage(Object obj)
-		{
-			return PlatformUI.getWorkbench().getSharedImages()
-					.getImage(ISharedImages.IMG_OBJ_ELEMENT);
-		}
-	}
 
 	/**
 	 * The constructor.
