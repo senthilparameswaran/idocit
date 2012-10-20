ee5a83be9cc50de3461372d8f3118fff14b7268e
krausedammann@web.de <krausedammann@web.de@165fa983-f9fe-27a1-bfdc-37165582f702>
2012-10-20 13:16:35 +0000

    Add log-statements into default-block of switch-statements

--- trunk/de.akra.idocit.wsdl/src/main/java/de/akra/idocit/wsdl/services/DocumentationParser.java
+++ trunk/de.akra.idocit.wsdl/src/main/java/de/akra/idocit/wsdl/services/DocumentationParser.java
@@ -43,7 +43,8 @@ import de.akra.idocit.core.utils.DescribedItemUtils;
  * @version 0.0.2
  * 
  */
-public class DocumentationParser {
+public class DocumentationParser
+{
 	private static final String HTML_TAG_TAB = "tab";
 
 	private static final String HTML_TAG_BR = "br";
@@ -60,9 +61,8 @@ public class DocumentationParser {
 	public static final String THEMATIC_GRID_ATTRIBUTE_NAME_GLOBAL = "global";
 
 	/**
-	 * The attribute name for the identifier of a signature element. The
-	 * signature element is identified by the path of types (the structure) to
-	 * the element.
+	 * The attribute name for the identifier of a signature element. The signature element
+	 * is identified by the path of types (the structure) to the element.
 	 */
 	public static final String SIGNATURE_ELEMENT_ATTRIBUTE_NAME = "signatureElement";
 
@@ -74,8 +74,7 @@ public class DocumentationParser {
 	/**
 	 * Logger.
 	 */
-	private static Logger logger = Logger.getLogger(DocumentationParser.class
-			.getName());
+	private static Logger logger = Logger.getLogger(DocumentationParser.class.getName());
 
 	// // use this for testing
 	// private static List<Addressee> supportedAddressees =
@@ -86,34 +85,33 @@ public class DocumentationParser {
 	// Collections.emptyList();
 
 	/**
-	 * Searches for the first documentation element in
-	 * <code>documentationElements</code> containing docpart elements and parses
-	 * the docpart elements.
+	 * Searches for the first documentation element in <code>documentationElements</code>
+	 * containing docpart elements and parses the docpart elements.
 	 * 
 	 * @param documentationElements
 	 *            List of documentation elements that should be parsed.
 	 * @return {@link List} of {@link Documentation}s; one {@link Documentation}
-	 *         represents one docpart element. If no documentation element with
-	 *         docpart elements is found, {@link Collections#emptyList()} is
-	 *         returned.
+	 *         represents one docpart element. If no documentation element with docpart
+	 *         elements is found, {@link Collections#emptyList()} is returned.
 	 */
-	public static List<Documentation> parseDocElements(
-			List<Element> documentationElements) {
+	public static List<Documentation> parseDocElements(List<Element> documentationElements)
+	{
 		List<Documentation> documentations = Collections.emptyList();
-		if (documentationElements != null) {
+		if (documentationElements != null)
+		{
 			Element docElem = findDocElemWithDocpart(documentationElements);
 
-			if (docElem != null) {
+			if (docElem != null)
+			{
 				NodeList docParts = docElem
 						.getElementsByTagName(DocumentationParser.DOCPART_TAG_NAME);
 
-				documentations = new ArrayList<Documentation>(
-						docParts.getLength());
+				documentations = new ArrayList<Documentation>(docParts.getLength());
 
-				for (int i = 0; i < docParts.getLength(); i++) {
+				for (int i = 0; i < docParts.getLength(); i++)
+				{
 					Node dPart = docParts.item(i);
-					Documentation doc = DocumentationParser
-							.parseDocPartElement(dPart);
+					Documentation doc = DocumentationParser.parseDocPartElement(dPart);
 					documentations.add(doc);
 				}
 			}
@@ -123,68 +121,77 @@ public class DocumentationParser {
 	}
 
 	/**
-	 * Searches for the first documentation element in <code>docElems</code>
-	 * with docpart elements as children.
+	 * Searches for the first documentation element in <code>docElems</code> with docpart
+	 * elements as children.
 	 * 
 	 * @param docElems
 	 *            List of documentation {@link Element}s.
-	 * @return The first documentation {@link Element} with docpart elements as
-	 *         children. If no element was found, <code>null</code> is returned.
+	 * @return The first documentation {@link Element} with docpart elements as children.
+	 *         If no element was found, <code>null</code> is returned.
 	 */
-	public static Element findDocElemWithDocpart(List<Element> docElems) {
+	public static Element findDocElemWithDocpart(List<Element> docElems)
+	{
 		return findElementWithName(docElems, DOCPART_TAG_NAME);
 	}
 
 	/**
-	 * Searches for the first documentation element in <code>docElems</code>
-	 * with thematic elements as children.
+	 * Searches for the first documentation element in <code>docElems</code> with thematic
+	 * elements as children.
 	 * 
 	 * @param docElems
 	 *            List of documentation {@link Element}s.
-	 * @return The first documentation {@link Element} with thematicgrid
-	 *         elements as children. If no element was found, <code>null</code>
-	 *         is returned.
+	 * @return The first documentation {@link Element} with thematicgrid elements as
+	 *         children. If no element was found, <code>null</code> is returned.
 	 */
-	public static Element findDocElemWithThematicGrid(List<Element> docElems) {
+	public static Element findDocElemWithThematicGrid(List<Element> docElems)
+	{
 		return findElementWithName(docElems, THEMATIC_GRID_TAG_NAME);
 	}
 
 	/**
 	 * Returns the name of the thematic grid in the given documentation-element.
 	 * 
-	 * Please note: This method returns the name of the first thematic grid
-	 * found in the list of documentation's children.
+	 * Please note: This method returns the name of the first thematic grid found in the
+	 * list of documentation's children.
 	 * 
 	 * @param docElemWithThematicGrid
 	 *            The &lt;documentation&gt;-element containing the thematic grid.
 	 * 
 	 * @return The name of the first found thematic grid
 	 */
-	public static String readThematicGridName(Element docElemWithThematicGrid) {
-		NodeList documentationChildren = docElemWithThematicGrid
-				.getChildNodes();
+	public static String readThematicGridName(Element docElemWithThematicGrid)
+	{
+		NodeList documentationChildren = docElemWithThematicGrid.getChildNodes();
 
 		int children = documentationChildren.getLength();
 
-		for (int i = 0; i < children; i++) {
+		for (int i = 0; i < children; i++)
+		{
 			Node child = documentationChildren.item(i);
 
-			if (child.getNodeName().equals(THEMATIC_GRID_TAG_NAME)) {
+			if (child.getNodeName().equals(THEMATIC_GRID_TAG_NAME))
+			{
 				NamedNodeMap attributes = child.getAttributes();
 
-				if (attributes != null) {
+				if (attributes != null)
+				{
 					Node nameAttribute = attributes
 							.getNamedItem(THEMATIC_GRID_ATTRIBUTE_NAME);
 
-					if (nameAttribute != null) {
+					if (nameAttribute != null)
+					{
 						return nameAttribute.getNodeValue();
-					} else {
+					}
+					else
+					{
 						// It seems that we have an incomplete
 						// <thematicgrid>-element. This is why we don't continue
 						// to search for a named one.
 						return null;
 					}
-				} else {
+				}
+				else
+				{
 					// It seems that we have an incomplete
 					// <thematicgrid>-element. This is why we don't continue to
 					// search for a named one.
@@ -197,28 +204,31 @@ public class DocumentationParser {
 	}
 
 	/**
-	 * Searches for the first documentation element in <code>docElems</code>
-	 * with docpart elements as children.
+	 * Searches for the first documentation element in <code>docElems</code> with docpart
+	 * elements as children.
 	 * 
 	 * @param docElems
 	 *            List of documentation {@link Element}s.
-	 * @return The first documentation {@link Element} with docpart elements as
-	 *         children. If no element was found, <code>null</code> is returned.
+	 * @return The first documentation {@link Element} with docpart elements as children.
+	 *         If no element was found, <code>null</code> is returned.
 	 */
-	public static Element findElementWithName(List<Element> docElems,
-			String elementName) {
-		if (docElems != null && !docElems.isEmpty()) {
+	public static Element findElementWithName(List<Element> docElems, String elementName)
+	{
+		if (docElems != null && !docElems.isEmpty())
+		{
 			// search for the first <documentation> element with <docpart>
 			// elements
 			boolean bFoundDocParts = false;
 			Iterator<Element> it = docElems.iterator();
-			while (it.hasNext() && !bFoundDocParts) {
+			while (it.hasNext() && !bFoundDocParts)
+			{
 				Element docElem = it.next();
 				NodeList docParts = docElem.getElementsByTagName(elementName);
 
-				if (docParts.getLength() > 0) {
-					logger.log(Level.FINE, "Number of <docpart> elements = "
-							+ docParts.getLength());
+				if (docParts.getLength() > 0)
+				{
+					logger.log(Level.FINE,
+							"Number of <docpart> elements = " + docParts.getLength());
 
 					return docElem;
 				}
@@ -232,44 +242,43 @@ public class DocumentationParser {
 	 * documentations and the attributes
 	 * {@link DocumentationParser#THEMATIC_ROLE_ATTRIBUTE_NAME},
 	 * {@link DocumentationParser#THEMATIC_SCOPE_ATTRIBUTE_NAME} and
-	 * {@link DocumentationParser#SIGNATURE_ELEMENT_ATTRIBUTE_NAME} of the
-	 * docpart and returns a new {@link Documentation} with this information. If
-	 * an attribute is not available or has an unknown value it is simply
-	 * ignored and not set.
+	 * {@link DocumentationParser#SIGNATURE_ELEMENT_ATTRIBUTE_NAME} of the docpart and
+	 * returns a new {@link Documentation} with this information. If an attribute is not
+	 * available or has an unknown value it is simply ignored and not set.
 	 * 
 	 * @param docpartElem
 	 *            docpart element that should be parsed.
 	 * @return A new {@link Documentation} with the parsed information.
 	 */
-	private static Documentation parseDocPartElement(Node docpartElem) {
+	private static Documentation parseDocPartElement(Node docpartElem)
+	{
 		Documentation doc = parseAddressees(docpartElem);
 
 		// read attributes
 		NamedNodeMap attributes = docpartElem.getAttributes();
 		Node attr;
-		if ((attr = attributes.getNamedItem(THEMATIC_ROLE_ATTRIBUTE_NAME)) != null) {
+		if ((attr = attributes.getNamedItem(THEMATIC_ROLE_ATTRIBUTE_NAME)) != null)
+		{
 			logger.log(Level.FINE,
-					THEMATIC_ROLE_ATTRIBUTE_NAME + "=\"" + attr.getNodeValue()
-							+ "\"");
+					THEMATIC_ROLE_ATTRIBUTE_NAME + "=\"" + attr.getNodeValue() + "\"");
 
-			ThematicRole role = DescribedItemUtils.findThematicRole(attr
-					.getNodeValue());
+			ThematicRole role = DescribedItemUtils.findThematicRole(attr.getNodeValue());
 			doc.setThematicRole(role);
 		}
 
 		if ((attr = attributes
-				.getNamedItem(DocumentationParser.SIGNATURE_ELEMENT_ATTRIBUTE_NAME)) != null) {
-			logger.log(Level.FINE,
-					DocumentationParser.SIGNATURE_ELEMENT_ATTRIBUTE_NAME
-							+ "=\"" + attr.getNodeValue() + "\"");
+				.getNamedItem(DocumentationParser.SIGNATURE_ELEMENT_ATTRIBUTE_NAME)) != null)
+		{
+			logger.log(Level.FINE, DocumentationParser.SIGNATURE_ELEMENT_ATTRIBUTE_NAME
+					+ "=\"" + attr.getNodeValue() + "\"");
 			doc.setSignatureElementIdentifier(attr.getNodeValue());
 		}
-		
+
 		if ((attr = attributes
-				.getNamedItem(DocumentationParser.ERROR_DOCUMENTATION_ATTRIBUTE_NAME)) != null) {
-			logger.log(Level.FINE,
-					DocumentationParser.ERROR_DOCUMENTATION_ATTRIBUTE_NAME
-							+ "=\"" + attr.getNodeValue() + "\"");
+				.getNamedItem(DocumentationParser.ERROR_DOCUMENTATION_ATTRIBUTE_NAME)) != null)
+		{
+			logger.log(Level.FINE, DocumentationParser.ERROR_DOCUMENTATION_ATTRIBUTE_NAME
+					+ "=\"" + attr.getNodeValue() + "\"");
 			doc.setErrorCase(Boolean.parseBoolean(attr.getNodeValue()));
 		}
 
@@ -282,10 +291,10 @@ public class DocumentationParser {
 	 * 
 	 * @param docpartElem
 	 *            The docpart element that should be parsed.
-	 * @return A new {@link Documentation} with the documentations for the
-	 *         addresses.
+	 * @return A new {@link Documentation} with the documentations for the addresses.
 	 */
-	private static Documentation parseAddressees(Node docpartElem) {
+	private static Documentation parseAddressees(Node docpartElem)
+	{
 		Documentation doc = new Documentation();
 		Map<Addressee, String> docMap = doc.getDocumentation();
 		List<Addressee> addresseeSequence = doc.getAddresseeSequence();
@@ -293,21 +302,22 @@ public class DocumentationParser {
 		NodeList childList = docpartElem.getChildNodes();
 
 		// iterate all child nodes to find addressee elements
-		for (int i = 0; i < childList.getLength(); i++) {
+		for (int i = 0; i < childList.getLength(); i++)
+		{
 			Node item = childList.item(i);
 
 			// if found addressee element, add the documentation to the map
 			// in general there should be only addressee elements
-			if (item.getNodeName().equals(ADDRESSEE_ELEMENT_NAME)) {
+			if (item.getNodeName().equals(ADDRESSEE_ELEMENT_NAME))
+			{
 				NamedNodeMap attributes = item.getAttributes();
-				Node attribute = attributes
-						.getNamedItem(ADDRESSEE_GROUP_ATTRIBUTE_NAME);
+				Node attribute = attributes.getNamedItem(ADDRESSEE_GROUP_ATTRIBUTE_NAME);
 
-				Addressee addressee = DescribedItemUtils
-						.findAddressee(attribute.getNodeValue());
-				logger.log(Level.FINE, attribute.getNodeName() + " -> "
-						+ attribute.getNodeValue() + " | Addressee="
-						+ addressee.getName());
+				Addressee addressee = DescribedItemUtils.findAddressee(attribute
+						.getNodeValue());
+				logger.log(Level.FINE,
+						attribute.getNodeName() + " -> " + attribute.getNodeValue()
+								+ " | Addressee=" + addressee.getName());
 
 				String text = readTextFromAddresseElement(item);
 
@@ -317,10 +327,12 @@ public class DocumentationParser {
 				// save sequence of adding addressees
 				addresseeSequence.add(addressee);
 
-			} else {
-				logger.log(Level.FINE, "Expect <" + ADDRESSEE_ELEMENT_NAME
-						+ ">, but found: name=" + item.getNodeName()
-						+ ", type=" + item.getNodeType());
+			}
+			else
+			{
+				logger.log(Level.FINE,
+						"Expect <" + ADDRESSEE_ELEMENT_NAME + ">, but found: name="
+								+ item.getNodeName() + ", type=" + item.getNodeType());
 			}
 		}
 
@@ -328,33 +340,45 @@ public class DocumentationParser {
 	}
 
 	/**
-	 * Reads the child nodes from the addressee element and converts the &lt;br
-	 * /&gt; and &lt;tab /&gt; elements to the corresponding ASCII characters.
-	 * Unnecessary newlines and tabulators are removed from the text. After the
-	 * text is build {@link String#trim()} is run on it.
+	 * Reads the child nodes from the addressee element and converts the &lt;br /&gt; and
+	 * &lt;tab /&gt; elements to the corresponding ASCII characters. Unnecessary newlines
+	 * and tabulators are removed from the text. After the text is build
+	 * {@link String#trim()} is run on it.
 	 * 
 	 * @param addresseeElement
 	 *            The addressee element from which the text should be read.
 	 * @return The normalized text.
 	 */
-	private static String readTextFromAddresseElement(Node addresseeElement) {
+	private static String readTextFromAddresseElement(Node addresseeElement)
+	{
 		StringBuilder text = new StringBuilder();
 		NodeList nodes = addresseeElement.getChildNodes();
 
-		for (int i = 0; i < nodes.getLength(); ++i) {
+		for (int i = 0; i < nodes.getLength(); ++i)
+		{
 			Node node = nodes.item(i);
-			switch (node.getNodeType()) {
+			switch (node.getNodeType())
+			{
 			case Node.TEXT_NODE:
-				String unescapedText = StringEscapeUtils.unescapeHtml4(node.getNodeValue());
+				String unescapedText = StringEscapeUtils.unescapeHtml4(node
+						.getNodeValue());
 				text.append(StringUtils.cleanFormatting(unescapedText));
 				break;
 			case Node.ELEMENT_NODE:
-				if (node.getNodeName().equalsIgnoreCase(HTML_TAG_BR)) {
+				if (node.getNodeName().equalsIgnoreCase(HTML_TAG_BR))
+				{
 					text.append(System.getProperty("line.separator"));
-				} else if (node.getNodeName().equalsIgnoreCase(HTML_TAG_TAB)) {
+				}
+				else if (node.getNodeName().equalsIgnoreCase(HTML_TAG_TAB))
+				{
 					text.append('\t');
 				}
 				break;
+			default:
+			{
+				// Do nothing!
+				logger.info("The node-type is " + node.getNodeType());
+			}
 			}
 		}
 		return text.toString().trim();
