263706ee7fa5367de5b463a8e935196fa519861d
krausedammann <krausedammann@web.de>
2012-11-18 19:59:42 +0000

    Releases 0.0.9 and 0.0.10 were left out due to harmonization of version numbers

---
+++ de.akra.idocit.common.tests/src/test/java/de/akra/idocit/common/services/ThematicGridServiceTest.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright 2012 AKRA GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package de.akra.idocit.common.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ThematicGridServiceTest
+{
+
+	@Test
+	public void testExtractVerb()
+	{
+		// Positive tests
+		// ******************************************************************************
+		{
+			// Test case #1: the extracted verb is "remove" from the identifier
+			// "removeName".
+			assertEquals("remove", ThematicGridService.extractVerb("removeName"));
+
+			// Test case #2: the extracted verb is "remove" from the identifier
+			// "RemoveName".
+			assertEquals("remove", ThematicGridService.extractVerb("RemoveName"));
+
+			// Test case #3: the extracted verb is null for the identifier "".
+			assertEquals(null, ThematicGridService.extractVerb(""));
+
+			// Test case #4: the extracted verb is null for the identifier "   ".
+			assertEquals(null, ThematicGridService.extractVerb("  "));
+		}
+
+		// Negative tests
+		// ******************************************************************************
+		{
+			// None
+		}
+	}
+
+}
